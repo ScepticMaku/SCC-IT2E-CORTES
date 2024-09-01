efe729025cc7689bbdd0ec8775e4e0bede833762
Cortes, Mark Jay Vincent S. <markjayvincentcortes@gmail.com>
2024-09-01 20:07:54 +0800

    Another changes

--- src/cortes/Cortes.java
+++ src/cortes/Cortes.java
@@ -5,6 +5,7 @@ public class Cortes {
     public static void main(String[] args) {
         Grade grd = new Grade();
         Sales sl = new Sales();
+        Product pr = new Product();
         
         grd.getGrades();
     }
